23b70df3fee5b3fe835533a9c094a7136afde856
DAO-CHO <lndao.httt0117@student.ctuet.edu.vn>
2022-04-04 00:13:57 +0700

    hiệu chỉnh delete item.

---
+++ app/src/main/java/com/example/admin/augscan/DialogDeleteActivity.java
@@ -0,0 +1,58 @@
+package com.example.admin.augscan;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class DialogDeleteActivity extends Activity {
+    private TextView text;
+    DatabaseReference mdatabaseReference;
+    private FirebaseAuth firebaseAuth;
+    public String getItemBarcode;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_delete);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        final FirebaseUser users = firebaseAuth.getCurrentUser();
+        String finaluser = users.getEmail();
+        String resultemail = finaluser.replace(".","");
+        mdatabaseReference = FirebaseDatabase.getInstance().getReference("Users").child(resultemail);
+        text = findViewById(R.id.textView);
+        Intent n = getIntent();
+        getItemBarcode = n.getStringExtra("itembarcode");
+        Query firebaseSearchQuery = mdatabaseReference.child("Items").child(getItemBarcode);
+        firebaseSearchQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    mdatabaseReference.child("Items").child(getItemBarcode).removeValue();
+                    text.setText( "'" + getItemBarcode + "'" +" item has been successfully deleted");
+                } else {
+                    text.setText("delete failed");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/admin/augscan/deleteItemsActivity.java
+++ app/src/main/java/com/example/admin/augscan/deleteItemsActivity.java
@@ -1,24 +1,34 @@
 package com.example.admin.augscan;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class deleteItemsActivity extends AppCompatActivity {
-    public static TextView resultdeleteview;
+    public static EditText resultSearchView;
+    public static TextView resultdeleteview, item_barcode, item_name;
     private FirebaseAuth firebaseAuth;
-    Button scantodelete, deletebtn;
+    public static Button btnDelete;
+    Button searchBtn;
     DatabaseReference databaseReference;
+    RecyclerView mrecyclerview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,39 +36,84 @@ public class deleteItemsActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
 
         databaseReference = FirebaseDatabase.getInstance().getReference("Users");
-        resultdeleteview = findViewById(R.id.barcodedelete);
-        scantodelete = findViewById(R.id.buttonscandelete);
-        deletebtn= findViewById(R.id.deleteItemToTheDatabasebtn);
+        searchBtn = findViewById(R.id.searchBtn);
+        resultSearchView = findViewById(R.id.searchField);
+        item_barcode = (TextView) findViewById(R.id.viewitembarcode);
+        item_name = (TextView) findViewById(R.id.viewitemname);
+        mrecyclerview = findViewById(R.id.recyclerViewsDelete);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        mrecyclerview.setLayoutManager(manager);
+        mrecyclerview.setHasFixedSize(true);
+        mrecyclerview.setLayoutManager(new LinearLayoutManager(this));
 
-        scantodelete.setOnClickListener(new View.OnClickListener() {
+        searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), ScanCodeActivitydel.class));
+                String searchText = resultSearchView.getText().toString();
+                resultSearch(searchText);
             }
         });
-
-        deletebtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                deletefrmdatabase();
-            }
-        });
-
     }
 
-    public void deletefrmdatabase()
-    {
-        String deletebarcodevalue = resultdeleteview.getText().toString();
-        final FirebaseUser users = firebaseAuth.getCurrentUser();
-        String finaluser=users.getEmail();
-        String resultemail = finaluser.replace(".","");
+    public void resultSearch(String searchtext) {
 
-        if(!TextUtils.isEmpty(deletebarcodevalue)){
-            databaseReference.child(resultemail).child("Items").child(deletebarcodevalue).removeValue();
-            Toast.makeText(deleteItemsActivity.this,"Item is Deleted",Toast.LENGTH_SHORT).show();
+        final FirebaseUser users = firebaseAuth.getCurrentUser();
+        String finaluser = users.getEmail();
+        String resultemail = finaluser.replace(".", "");
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(resultemail).child("Items");
+        Query firebaseSearchQuery = databaseReference.orderByChild("itembarcode").startAt(searchtext).endAt(searchtext + "\uf8ff");
+        if (!TextUtils.isEmpty(searchtext)) {
+            FirebaseRecyclerAdapter<Items, deleteItemsActivity.UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Items, deleteItemsActivity.UsersViewHolder>
+
+                    (Items.class,
+                            R.layout.list_item_delete_layout,
+                            deleteItemsActivity.UsersViewHolder.class,
+                            firebaseSearchQuery) {
+                @Override
+                protected void populateViewHolder(deleteItemsActivity.UsersViewHolder viewHolder, Items model, int position) {
+
+                    viewHolder.setDetails(getApplicationContext(), model.getItembarcode(), model.getItemcategory(), model.getItemname(), model.getItemprice(), model.getItemimg());
+
+                }
+            };
+            mrecyclerview.setAdapter(firebaseRecyclerAdapter);
+            Toast.makeText(deleteItemsActivity.this, "Wait a minute", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(deleteItemsActivity.this, "Please scan Barcode", Toast.LENGTH_SHORT).show();
         }
-        else{
-            Toast.makeText(deleteItemsActivity.this,"Please scan Barcode",Toast.LENGTH_SHORT).show();
+    }
+
+
+    public static class UsersViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        private Items itemList = new Items();
+
+        public void setDetails(Context ctx, final String itembarcode, String itemcategory, String itemname, String itemprice, String itemimg) {
+            TextView item_barcode = (TextView) mView.findViewById(R.id.viewitembarcode);
+            TextView item_name = (TextView) mView.findViewById(R.id.viewitemname);
+            TextView item_category = (TextView) mView.findViewById(R.id.viewitemcategory);
+            TextView item_price = (TextView) mView.findViewById(R.id.viewitemprice);
+            item_barcode.setText(itembarcode);
+            item_category.setText(itemcategory);
+            item_name.setText(itemname);
+            item_price.setText(itemprice);
+            btnDelete = mView.findViewById(R.id.buttonDelete);
+            this.itemList.setItem(itemname, itemcategory, itemprice, itembarcode, itemimg);
+            btnDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i;
+                    i = new Intent(mView.getContext(), DialogDeleteActivity.class);
+                    i.putExtra("itembarcode", itemList.getItembarcode());
+                    view.getContext().startActivity(i);
+                }
+            });
         }
     }
 }
